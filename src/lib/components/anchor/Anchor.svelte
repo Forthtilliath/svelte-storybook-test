ae68e0f8d1b833b69f2975f9512f947d7fa5bb8c
Forthtilliath <forth@live.fr>
2024-05-10 22:45:49 +0200

    add anchor

---
+++ src/lib/components/anchor/Anchor.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import '../../../app.css';
+	import { cn } from '$lib/utils.js';
+	import type { SvelteComponent } from 'svelte';
+	import type { HTMLAnchorAttributes } from 'svelte/elements';
+	import { type Size, type Variant, anchorVariants } from './index.js';
+
+	interface $$Props extends HTMLAnchorAttributes {
+		class?: string;
+		variant?: Variant;
+		size?: Size;
+		content?: string | SvelteComponent;
+	}
+
+	let className: $$Props['class'] = '';
+	export let variant: $$Props['variant'] = 'default';
+	export let size: $$Props['size'] = 'default';
+	export let content: $$Props['content'] = 'Button';
+	export let href: $$Props['href'] = '#';
+	export { className as class };
+</script>
+
+<a class={cn(anchorVariants({ variant, size, className }))} {href} {...$$restProps}>
+	<slot>
+		{content}
+	</slot>
+</a>
---
+++ src/lib/components/anchor/anchor.stories.svelte
@@ -0,0 +1,78 @@
+<script lang="ts" context="module">
+	import type { Meta } from '@storybook/svelte';
+	import Anchor from './Anchor.svelte';
+	import '../../../app.css';
+
+	export const meta = {
+		title: 'Components/With Tailwind + CSF/Anchor',
+		component: Anchor,
+		tags: ['autodocs'],
+		argTypes: {
+			class: { control: 'text', type: 'string' },
+			size: {
+				control: 'select',
+				options: ['default', 'sm', 'lg']
+			},
+			variant: {
+				control: 'select',
+				options: ['default', 'destructive', 'outline', 'primary', 'secondary', 'ghost', 'disabled']
+			},
+			content: {
+				control: 'text'
+			},
+			href: {
+				control: 'text'
+			}
+		}
+	} satisfies Meta<Anchor>;
+</script>
+
+<script lang="ts">
+	import { Story } from '@storybook/addon-svelte-csf';
+	import EnvelopeOpen from 'svelte-radix/EnvelopeOpen.svelte';
+	import { ChevronRight, Reload } from 'svelte-radix';
+</script>
+
+<Story name="Default">
+	<Anchor />
+</Story>
+
+<Story name="Primary">
+	<Anchor variant="primary" />
+</Story>
+
+<Story name="Secondary">
+	<Anchor variant="secondary" />
+</Story>
+
+<Story name="Destructive">
+	<Anchor variant="destructive" />
+</Story>
+
+<Story name="Outline">
+	<Anchor variant="outline" />
+</Story>
+
+<Story name="Ghost">
+	<Anchor variant="ghost" />
+</Story>
+
+<Story name="With Icon">
+	<Anchor>
+		<EnvelopeOpen class="mr-2 h-5 w-5" />
+		With Icon
+	</Anchor>
+</Story>
+
+<Story name="Icon">
+	<Anchor size="icon">
+		<ChevronRight class="h-4 w-4" />
+	</Anchor>
+</Story>
+
+<Story name="Disabled">
+	<Anchor variant="disabled">
+		<Reload class="mr-2 h-4 w-4 animate-spin" />
+		Please wait
+	</Anchor>
+</Story>
