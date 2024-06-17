49c00ce91a02fbeb46bd55c8784511706b413b8f
Forthtilliath <forth@live.fr>
2024-06-17 14:48:43 +0200

    chore: move input & label into separated folders

--- src/lib/components/field-v2/field-input/field-input.svelte
+++ src/lib/components/field-v2/field-input/field-input.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import Label from './label.svelte';
-	import Input from './input.svelte';
+	import { Label } from './label';
+	import { Input } from './input';
 	import { type Props } from './index.js';
 	import { cn, uniqueId } from './utils';
 
